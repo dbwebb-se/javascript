71b2469cc76b0dbb54f92072d29a2a7889580087
Mikael Roos <mikael.t.h.roos@gmail.com>
2016-10-24 09:35:46 +0200

    fix validation messages for dbwebb validate

--- example/jslint/passes.js
+++ example/jslint/passes.js
@@ -5,14 +5,14 @@
 
     function helloWorld() {
         console.log("called function helloWorld()");
-        alert("Hello World from a JavaScript function showing an alert.");
+        window.alert("Hello World from a JavaScript function showing an alert.");
     }
 
     function helloMyNumber() {
         console.log("called function helloMyNumber()");
         var matched;
         var max = 42;
-        var yourLuckyNumber = prompt("So, whats your lucky number today (enter a number between 1 and " + max + ")?");
+        var yourLuckyNumber = window.prompt("So, whats your lucky number today (enter a number between 1 and " + max + ")?");
         var myLuckyNumber = Math.floor(Math.random() * (max + 1));
         var paragraph = document.getElementById("luckynumber");
 
@@ -21,5 +21,8 @@
             : "NOT ";
         paragraph.innerHTML = paragraph.innerHTML + " Your\"e lucky number is: " + yourLuckyNumber + ". Mine is: " + myLuckyNumber + ". They did " + matched + "match!";
     }
+
+    helloWorld();
+    helloMyNumber();
 
 }());
